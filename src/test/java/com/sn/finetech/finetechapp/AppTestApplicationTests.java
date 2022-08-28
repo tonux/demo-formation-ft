3c6b28a12897e662d2f27dece691f63a131ee8b1
lamine2000 <lemzogueye.lg72@gmail.com>
2022-08-28 21:55:44 +0000

    unit-tests written and passed By DepartmentRepository and PersonRepository

--- src/test/java/com/sn/finetech/finetechapp/AppTestApplicationTests.java
+++ src/test/java/com/sn/finetech/finetechapp/AppTestApplicationTests.java
@@ -1,12 +1,16 @@
 package com.sn.finetech.finetechapp;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class AppTestApplicationTests {
 
 	@Test
+	@DisplayName("should load the context")
 	void contextLoads() {
 	}
 
--- src/test/java/com/sn/finetech/finetechapp/repositories/PersonRepositoryTest.java
+++ src/test/java/com/sn/finetech/finetechapp/repositories/PersonRepositoryTest.java
@@ -1,18 +1,18 @@
 package com.sn.finetech.finetechapp.repositories;
 
 import com.sn.finetech.finetechapp.model.Person;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -25,40 +25,67 @@ class PersonRepositoryTest {
 
     @BeforeEach
     void init() {
-        // personRepository.deleteAll();
-        // personRepository.save(new Person("Moussa", "Moussa", "test@gmail.com", LocalDate.now()));
+        //create list of persons
+        List<Person> persons = List.of(
+                        new Person(
+                                "Moussa",
+                                "Gueye",
+                                "test@gmail.com",
+                                LocalDate.now()),
+
+                        new Person(
+                                "Moussa",
+                                "Fall",
+                                "fall@gmail.com",
+                                LocalDate.now()));
+
+        when(personRepository.findByFirstName(anyString()))
+                .thenReturn(persons);
+
+        when(personRepository.findByFirstNameAndLastName(anyString(), anyString()))
+                .thenReturn(List.of(persons.get(0)));
+
+        when(personRepository.findByBirthdayGreaterThan(any(LocalDate.class)))
+                .thenReturn(List.of());
+
+        when(personRepository.findByDepartmentCodeContainsI())
+                .thenReturn(List.of(persons.get(1)));
+
+        when(personRepository.findByLastName(anyString()))
+                .thenReturn(List.of(persons.get(0)));
     }
 
     @Test
     void findByFirstName() {
-        when(personRepository.findByFirstName(anyString()))
-                .thenReturn(List.of());
-        List<Person> persons = personRepository.findByFirstName("tonux");
+        List<Person> persons = personRepository.findByFirstName("Moussa");
+        assertNotNull(persons);
+        assertEquals(2, persons.size());
+    }
+
+    @Test
+    void findByFirstNameAndLastName() {
+        List<Person> persons = personRepository.findByFirstNameAndLastName("Moussa", "anystring");
+        assertNotNull(persons);
+        assertEquals(1, persons.size());
+    }
+
+    @Test
+    void findByBirthdayGreaterThan() {
+        List<Person> persons = personRepository.findByBirthdayGreaterThan(LocalDate.now());
         assertNotNull(persons);
         assertEquals(0, persons.size());
     }
 
     @Test
-    void findByFirstNameAndLastName() {
-    }
-
-    @Test
-    void findByBirthdayGreaterThan() {
-    }
-
-    @Test
-    void findByQuery() {
-    }
-
-    @Test
     void findByDepartmentCodeContainsI() {
+        List<Person> persons = personRepository.findByDepartmentCodeContainsI();
+        assertNotNull(persons);
+        assertEquals(1, persons.size());
     }
 
     @Test
     void findByLastName() {
-        when(personRepository.findByLastName(anyString()))
-                .thenReturn(new Person("Moussa", "Moussa", "test@gmail.com", LocalDate.now()));
-        Person person = personRepository.findByLastName("Moussa");
+        Person person = personRepository.findByLastName("Moussa").get(0);
         assertNotNull(person);
         assertEquals("Moussa", person.getLastName());
         assertEquals("test@gmail.com", person.getEmail());
